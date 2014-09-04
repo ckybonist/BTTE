9d7747058b2bac50e0dd5f7629d417633360011f
Kuan-Yu Chu <joechu152@gmail.com>
2014-09-04 14:32:31 +0800

    Move const variables to TransTime.h

---
+++ include/TransTime.h
@@ -0,0 +1,34 @@
+#ifndef _TRANSTIME_H
+#define _TRANSTIME_H
+
+#include "Piece.h"
+
+/**
+ * Simulation arguments
+ *
+ * Piece:
+ *     @piece_size: 512 (Kbytes)
+ *
+ * Category of peers:
+ *	   @class1 : 10 (Mbps)   // seeder
+ *	   @class2 : 128 (Kbps)  // leecher or normal peer
+ *	   @class3 : 56 (Kbps)   // leecher or normal peer
+ *
+ * */
+
+/* Download speed of each class */
+const int DOWNSPEED_1 = 10 * 1024 * 1024;
+const int DOWNSPEED_2 = 128 * 1024;
+const int DOWNSPEED_3 = 56 * 1024;
+
+/* Distributed rate(%) of each class */
+const int DRATE[3] = {30, 50, 20};
+
+const float TRANS_TIME[3]  // each class' transmission time
+{
+	(float)PIECE_SIZE / (float)DOWNSPEED_1,
+	(float)PIECE_SIZE / (float)DOWNSPEED_2,
+	(float)PIECE_SIZE / (float)DOWNSPEED_3,
+};
+
+#endif // for #ifndef _TRANSTIME_H
