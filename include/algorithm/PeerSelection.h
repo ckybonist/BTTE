286ea39f03d0d9f35f54a3295017d13b4f87bc0e
Kuan-Yu Chu <joechu152@gmail.com>
2014-09-05 01:11:12 +0800

    Create include/algorithm src/algorithm dir and move header file and cpp file of PeerSelection in it

--- include/algorithm/PeerSelection.h
+++ include/algorithm/PeerSelection.h
@@ -1,50 +1,48 @@
 #ifndef _PEERSELECTION_H
 #define _PEERSELECTION_H
 
-#include "Dice.h"
 #include "Peer.h"
 
 class IPeerSelection
 {
 public:
-    virtual Peer_t& choosePeers() = 0;
+    virtual int& choosePeers() = 0;
 
 protected:
     virtual float& allotPGDelay() = 0;
 };
 
+
 class Random : protected IPeerSelection
 {
 public:
-    Peer_t& choosePeers();
+    int& choosePeers();
 
 protected:
     float& allotPGDelay();
-
-private:
-    Dice dice;
 };
+
 
 class LoadBalance : protected IPeerSelection
 {
 public:
-    Peer_t& choosePeers();
+    int& choosePeers();
 
 protected:
     float& allotPGDelay();
 };
 
+
 class ClusterBased : protected IPeerSelection
 {
 public:
-    Peer_t& choosePeers();
+    int& choosePeers();
 
 protected:
     float& allotPGDelay();
 
 private:
     void setClusterID(Peer_t&);
-    Dice dice;
 };
 
 #endif // for #ifndef _PEERSELECTION_H
