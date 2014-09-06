7963fcabea6b2881e446a00de2b96d2b828814f5
Kuan-Yu Chu <joechu152@gmail.com>
2014-09-07 02:14:38 +0800

    Init rand seed of custom srand function which apply uniform distribution

--- src/BTTE.cpp
+++ src/BTTE.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 
+#include "Random.h"
 #include "EnvManager.h"
 
 using namespace std;
@@ -7,12 +8,19 @@ using namespace std;
 
 int main(int argc, const char *argv[])
 {
+    uniformdist::srand((unsigned)time(0));
+
     // Singleton init
-	//(InitEnv::getInstance()).createPeers();
+	(EnvManager::getInstance()).init("../btte.conf");
 
+    // Do something
+
+	(EnvManager::getInstance()).destroy();
+
+    /*
     // Normal init
-	//InitEnv init;
-	//init.createPeers(peers, N);
-
+	InitEnv init;
+	init.createPeers(peers, N);
+    */
 	return 0;
 }
