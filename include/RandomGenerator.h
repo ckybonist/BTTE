d97c5e6dcb1e57156612e21a9c89cd4725d62602
Kuan-Yu Chu <joechu152@gmail.com>
2014-09-06 18:06:26 +0800

    Add new rand() and srand() for uniform distribution

---
+++ include/RandomGenerator.h
@@ -0,0 +1,27 @@
+#ifndef _RANDOMGENERATOR_H
+#define _RANDOMGENERATOR_H
+
+#include <cstdlib>
+
+//const long long RANDMAX = 2147483647;  // C++ RAND_MAX Macro
+const long long INIT_NUM = 1491701989;
+long long rand_num = INIT_NUM;
+
+namespace uniformdist  // uniform distribution random
+{
+
+long long rand()
+{
+    /* X(n-1) * 7^5 % (2^31 - 1) */
+    rand_num = 16807 * rand_num % RAND_MAX;
+    return rand_num;
+}
+
+void srand(int seed)
+{
+    for(int i = 0; i < seed; i++)
+        rand_num = rand();
+}
+}
+
+#endif // for #ifndef _RANDOMGENERATOR_H
