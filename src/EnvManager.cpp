ac7230af5059b0c97eecd2af8bf49c6bd70d1d26
Kuan-Yu Chu <joechu152@gmail.com>
2014-09-07 02:58:13 +0800

    Read two arguments for deciding which algorithm to use in peer and piece selection

--- src/EnvManager.cpp
+++ src/EnvManager.cpp
@@ -22,6 +22,8 @@ void EnvManager::init(std::string configfile) const
     int num_peer = cfg.getValueOfKey<int>("NUM_PEER", 5000);  // if num_peer not being read, then set num_peer to 5000
     int num_seed = cfg.getValueOfKey<int>("NUM_SEED", 100);
     int num_leech = cfg.getValueOfKey<int>("NUM_LEECH", 100);
+    int type_peerSelect = cfg.getValueOfKey<int>("TYPE_PEERSELECT", 100);
+    int type_pieceSelect = cfg.getValueOfKey<int>("TYPE_PIECESELECT", 100);
 
     PeerManager pm(num_peer, num_seed, num_leech);
 
