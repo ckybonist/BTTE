a130b4d7edc3d3b289b83dd28732423c3109fa00
Kuan-Yu Chu <joechu152@gmail.com>
2014-09-07 02:20:45 +0800

    Change struct Piece_t
    1. Change members: no, is_receive to private
    2. Add public function to access and modify private members

--- include/Piece.h
+++ include/Piece.h
@@ -8,8 +8,17 @@ const int PIECE_SIZE = 512 * 1024 * 8;
 
 struct Piece_t
 {
-    std::string no;
-    bool received;
+    private:
+        int no;
+        bool is_receive;
+
+    public:
+        Piece_t() { int no = 0; is_receive = false; }
+        int getNo() { return no; }
+        bool getStatus() { return is_receive; }
+        void setNo(int val) { is_receive = val; }
+        void setStatus(bool status) { is_receive = status; }
+
 };
 
 Piece_t *makePieces(const int size);
