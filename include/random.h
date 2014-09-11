d3f9df78e4ddfe60172aedb9367f26e56d98ec61
Kuan-Yu Chu <joechu152@gmail.com>
2014-09-11 23:35:08 +0800

    Add name for arguments of function RangeRand

--- include/random.h
+++ include/random.h
@@ -2,12 +2,13 @@
 #define _UNIFORMRANDOM_H
 
 const long long RANDMAX = 2147483647;  // C++ RAND_MAX Macro
+
 extern long long g_rand_num;
 
 namespace uniformdist {  // uniform distribution random
 long long rand();
 void srand(int);
-int RangeRand(const int, const int);
+int RangeRand(const int low, const int up);
 }
 
 #endif // for #ifndef _RANDOMGENERATOR_H
