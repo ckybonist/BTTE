4144d0a6cee25bee63d69d09f749bc697bdbc170
Kuan-Yu Chu <joechu152@gmail.com>
2014-09-14 02:39:06 +0800

    Change return type of functions from ref to ptr

--- include/algorithm/peer_selection.h
+++ include/algorithm/peer_selection.h
@@ -6,43 +6,43 @@
 class IPeerSelection
 {
 public:
-    virtual int &ChoosePeers() = 0;
+    virtual int *ChoosePeers() = 0;
 
 protected:
-    virtual float &AllotPGDelay() = 0;
+    virtual float *AllotPGDelay() = 0;
 };
 
 
 class Random : protected IPeerSelection
 {
 public:
-    int &ChoosePeers();
+    int *ChoosePeers();
 
 protected:
-    float &AllotPGDelay();
+    float *AllotPGDelay();
 };
 
 
 class LoadBalance : protected IPeerSelection
 {
 public:
-    int &ChoosePeers();
+    int *ChoosePeers();
 
 protected:
-    float &AllotPGDelay();
+    float *AllotPGDelay();
 };
 
 
 class ClusterBased : protected IPeerSelection
 {
 public:
-    int& ChoosePeers();
+    int *ChoosePeers();
 
 protected:
-    float& AllotPGDelay();
+    float *AllotPGDelay();
 
 private:
-    void setClusterID(Peer&);
+    void setClusterID_(Peer&);
 };
 
 #endif // for #ifndef _PEERSELECTION_H
