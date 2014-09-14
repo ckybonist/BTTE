a4ef4c22a52ca3807fbdfa0766f345c35ac2f29f
Kuan-Yu Chu <joechu152@gmail.com>
2014-09-15 00:43:48 +0800

    Fix typo

--- src/peer.cpp
+++ src/peer.cpp
@@ -3,15 +3,16 @@
 Peer::Peer() {
     id = 0;
     cid = 0;  // not yet
-    time_per_piece = 0.0;
 
     is_seed = false;
     in_swarm = false;
 
     neighbors = nullptr;
-    pieces = nullptr;
 
-    pgdelay = nullptr;
+    pieces = nullptr;
+    time_per_piece = 0.0;
+
+    //pgdelay = nullptr;
     time = 0.0;
     counter = 0;
 }
