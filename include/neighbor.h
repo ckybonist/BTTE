3bff17ec6e805b470d989f50ebb6bfd00bf1865e
Kuan-Yu Chu <joechu152@gmail.com>
2014-09-17 00:06:47 +0800

    Add comment of pgdelay

--- include/neighbor.h
+++ include/neighbor.h
@@ -2,8 +2,8 @@
 #define _NEIGHBOR_H
 
 /*
- * @pgdelay: Using range 0 to 100 to represent
- *           the propagation delay (pgdelay).
+ * @pgdelay: Using range 0 to 100 to represent the propagation delay (pgdelay) (if neighbor list had been
+ *           changed, the pgdelay which peer ever had need to accumulate with current one).
  *
  * NOTE: Spliting the range to 4 class-intervals so that it could be use for determining
  *       that peers are in same cluster or not (CLUSTER-BASED PEER SELECTION algorithm).
