e3287e4fd715304b74fcee5e7ad808d5d56776bb
Kuan-Yu Chu <joechu152@gmail.com>
2014-09-20 15:48:29 +0800

    Remove unused code

--- include/peers_dict.h
+++
@@ -1,8 +0,0 @@
-#ifndef _PEERS_DICT_H
-#define _PEERS_DICT_H
-
-#include <map>
-
-extern std::map<int, int> g_pdict;
-
-#endif // for #ifndef _PEERS_DICT_H
--- src/peers_dict.cpp
+++
@@ -1,3 +0,0 @@
-#include "peers_dict.h"
-
-std::map<int, int> g_pdict = std::map<int, int>();
