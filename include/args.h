17a88b00ed190c122e7e18e1b9c38b04d99de12c
Kuan-Yu Chu <joechu152@gmail.com>
2014-09-27 17:13:32 +0800

    Add more arguments

--- include/args.h
+++ include/args.h
@@ -7,9 +7,13 @@ struct Args {
     int NUM_PEER;
     int NUM_SEED;
     int NUM_LEECH;
+    int NUM_PEERLIST;
+    int NUM_CHOKING;
+    int NUM_OU;
     int NUM_PIECE;
     int TYPE_PEERSELECT;
     int TYPE_PIECESELECT;
+    float ARRIVAL_RATE;
 
     Args() {};
     Args(const std::string filename);
