18eb2691d65851fa3c5af116e6089b43f6cb6c0b
Kuan-Yu Chu <joechu152@gmail.com>
2014-09-28 22:41:15 +0800

    Add src and header for piece selection (TODO)

---
+++ include/piece_selection.h
@@ -0,0 +1,12 @@
+#ifndef _PIECE_SELECTION_H
+#define _PIECE_SELECTION_H
+
+typedef enum
+{
+    RANDOM_FIRST_PIECE = 0,
+    RAREST_FIRST
+} TypePieceSelect;
+
+// TODO : algorithm
+
+#endif // for #ifndef _PIECE_SELECTION_H
---
+++ src/piece_selection.cpp
@@ -0,0 +1,1 @@
+#include "piece_selection.h"
