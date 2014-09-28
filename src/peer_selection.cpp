ab2086c16a900bdb537ea229296b3c9e728b57aa
Kuan-Yu Chu <joechu152@gmail.com>
2014-09-28 22:40:21 +0800

    Implment standard peer selection

---
+++ src/peer_selection.cpp
@@ -0,0 +1,91 @@
+#include "error.h"
+#include "random.h"
+
+#include "peer.h"
+#include "pg_delay.h"
+#include "peer_selection.h"
+
+
+using namespace uniformrand;
+
+
+namespace peerselection
+{
+
+IPeerSelect::IPeerSelect(const int NUM_PEERLIST, const int NUM_PEER)
+{
+   _NUM_PEERLIST = NUM_PEERLIST;
+   _NUM_PEER = NUM_PEER;
+   _ids = nullptr;
+   _pg_delays = nullptr;
+}
+
+Standard::~Standard()
+{
+    delete [] _ids;
+    delete [] _pg_delays;
+
+    for(int pid = 0; pid < _NUM_PEER; pid++)
+    {
+        delete [] g_peers[pid].neighbors;
+    }
+}
+
+Neighbor* Standard::SelectNeighbors()
+{
+    Neighbor* neighbors = new Neighbor[_NUM_PEERLIST];
+    if(neighbors == nullptr)
+    {
+        ExitError("Memory Allocation Fault in \
+                   peerselection::Standard::SelectNeighbors()");
+    }
+
+    if(nullptr == _ids && nullptr == _pg_delays)  // create array of random numbers first time
+    {
+        _ids = DistinctRandNumbers<int>(rsc_std_peerselect,
+                                  _NUM_PEERLIST,
+                                  _NUM_PEER);
+
+        _pg_delays = new int[_NUM_PEERLIST];
+
+        if(_ids == nullptr || _pg_delays == nullptr)
+        {
+            ExitError("Memory Allocation Fault in \
+                       peerselection::Standard::SelectNeighbors()");
+        }
+
+        for(int i = 0; i < _NUM_PEERLIST; i++)
+        {
+            _pg_delays[i] = Roll<int>(rsc_pgdelay, 1, 100);
+        }
+    }
+    else  // shuffle array if it had ever been created
+    {
+        Shuffle<int>(rsc_std_peerselect, _ids, _NUM_PEERLIST);
+
+        Shuffle<int>(rsc_std_peerselect, _pg_delays, _NUM_PEERLIST);
+    }
+
+    for(int i = 0; i < _NUM_PEERLIST; i++)
+    {
+        neighbors[i].id = _ids[i];
+        neighbors[i].pg_delay = _pg_delays[i];
+    }
+
+    return neighbors;
+}
+
+
+Neighbor* LoadBalancing::SelectNeighbors()
+{
+    //TODO
+    return nullptr;
+}
+
+Neighbor* ClusterBased::SelectNeighbors()
+{
+    //TODO
+    return nullptr;
+}
+
+}
