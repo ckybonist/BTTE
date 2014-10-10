7a5960fc8f4dd0141f39369ba74c3c8c4e8b9b17
Kuan-Yu Chu <joechu152@gmail.com>
2014-10-10 14:10:24 +0800

    Change function names in debug.cpp

--- src/debug.cpp
+++ src/debug.cpp
@@ -9,12 +9,12 @@ using std::cout;
 using std::endl;
 
 
-static void PntPeerInfo(const int pid);
-static void PntPieceInfo(const int pid,
+static void PeerInfo(const int pid);
+static void PieceInfo(const int pid,
                          const int NUM_PIECE,
                          const int NUM_SEED,
                          int (&counter)[10]);
-static void PntNeighborInfo(const int pid, const int NUM_PEERLIST);
+static void NeighborInfo(const int pid, const int NUM_PEERLIST);
 
 
 void ShowDbgInfo(const Args &args)
@@ -32,11 +32,11 @@ void ShowDbgInfo(const Args &args)
 
     for(int pid = 0; pid < args.NUM_PEER; pid++)
     {
-        PntPeerInfo(pid);
+        PeerInfo(pid);
 
-        PntPieceInfo(pid, args.NUM_PIECE, args.NUM_SEED, piece_own_counter);
+        PieceInfo(pid, args.NUM_PIECE, args.NUM_SEED, piece_own_counter);
 
-        PntNeighborInfo(pid, args.NUM_PEERLIST);
+        NeighborInfo(pid, args.NUM_PEERLIST);
 
         ///////////////////////////////////////
         // count how many peers have  each piece
@@ -65,14 +65,14 @@ void ShowDbgInfo(const Args &args)
     //delete [] piece_own_counter;
 }
 
-static void PntPeerInfo(const int pid)
+static void PeerInfo(const int pid)
 {
     ////////////////////////
     // id info
     cout << " 1. Peer ID: " << g_peers[pid].id << endl;
     cout << " 2. Cluster ID: " << g_peers[pid].cid << endl;
 
-    cout << "\n 6. Join time (not yet): " << g_peers[pid].start_time << endl;
+    cout << "\n 6. Join time (not yet): " << g_peers[pid].join_time << endl;
 
     if (true == g_peers[pid].is_seed)
     {
@@ -87,10 +87,10 @@ static void PntPeerInfo(const int pid)
         cout << " 3. Peer catagory: Average" << endl;
     }
 
-    cout << "\n 4. Time per piece: " << g_peers[pid].time_per_piece << endl;
+    cout << "\n 4. Time per packet: " << g_peers[pid].time_packet << endl;
 }
 
-static void PntPieceInfo(const int pid,
+static void PieceInfo(const int pid,
                          const int NUM_PIECE,
                          const int NUM_SEED,
                          int (&counter)[10])
@@ -115,7 +115,7 @@ static void PntPieceInfo(const int pid,
     cout << "    * Not yet: " << (NUM_PIECE - piece_count) << endl;
 }
 
-static void PntNeighborInfo(const int pid, const int NUM_PEERLIST)
+static void NeighborInfo(const int pid, const int NUM_PEERLIST)
 {
     /////////////////////////
     // neighbors info
