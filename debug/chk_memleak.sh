e1aa915ee31ac67ff19d7ff08b7c50be8a041624
ChuKuanYu <joechu1592@gmail.com>
2014-11-27 00:39:26 +0800

    Change file name of debug script

--- debug/chk_memleak.sh
+++ debug/chk_memleak.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-/usr/bin/valgrind --leak-check=full --track-origins=yes --log-file=leak.log ../bin/BTTE btte.conf.dbg
+if [ -f ../bin/BTTE ]; then
+    /usr/bin/valgrind --leak-check=full --track-origins=yes --log-file=leak.log ../bin/BTTE btte.conf.dbg
+else
+    echo "No binary found"
+    exit 1
+fi
