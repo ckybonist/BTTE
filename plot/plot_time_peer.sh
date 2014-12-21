797248df68661993bec617c33459f76f13ada6dc
ChuKuanYu <joechu1592@gmail.com>
2014-12-22 04:56:26 +0800

    Force running script by python3

--- plot/plot_time_peer.sh
+++ plot/plot_time_peer.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-./genr_plot_dat.py 'time_peer'
+python3 genr_plot_dat.py 'time_peer'
 /usr/bin/gnuplot -e "dat_prefix='output/time_peer_'; outfile='output/time_peer.png'; xlabelname='peer'" plot.plg
--- plot/plot_time_piece.sh
+++ plot/plot_time_piece.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-./genr_plot_dat.py 'time_piece'
+python3 genr_plot_dat.py 'time_piece'
 /usr/bin/gnuplot -e "dat_prefix='output/time_piece_'; outfile='output/time_piece.png'; xlabelname='piece'" plot.plg
