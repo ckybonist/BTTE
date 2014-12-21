e650139230375cca152ff6ab4c2fc4b3ed9feaa0
ChuKuanYu <joechu1592@gmail.com>
2014-12-21 17:28:15 +0800

    Fix problem when determing path of project_root
    Fix problem that path of project-root will change by
    where user executing script.

---
+++ script/compile.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+# Check arguments
+if [ $# -ge 1 ]; then
+    BUILD_FLAG=$1
+    if [[ "$BUILD_FLAG" != "debug" &&
+          "$BUILD_FLAG" != "release" &&
+          "$BUILD_FLAG" != "makeall" ]]; then
+        echo "First Argument: debug, relase, makeall, \"\" "
+        exit 1
+    fi
+else
+    echo "Wrong number of arguments"
+    exit 1
+fi
+
+# Build project
+PROJ_ROOT=$(readlink -f $(pwd))
+if [ $(basename $PROJ_ROOT) != 'BTTE' ]; then
+    PROJ_ROOT=$(dirname $PROJ_ROOT)
+fi
+
+BUILD_DIR="$PROJ_ROOT/build"
+
+if [ ! -d $BUILD_DIR ]; then
+    ./init_project $BUILD_FLAG
+else
+    if [ $BUILD_FLAG == "makeall" ]; then
+        cd $BUILD_DIR/debug
+        make clean && make
+        cd ../release
+        make clean && make
+    else
+        cd $BUILD_DIR/$BUILD_FLAG
+        make clean && make
+    fi
+fi
+
