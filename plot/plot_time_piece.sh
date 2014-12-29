fc5d7a1bb8b7ed31f2578bdaaa2e46a1f7b1ba1f
ChuKuanYu <joechu1592@gmail.com>
2014-12-29 20:03:57 +0800

    Add option that user can decide which algorithm is the comparative object of simulation

--- plot/plot_time_piece.sh
+++ plot/plot_time_piece.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 python3 genr_plot_dat.py 'time_piece'
-/usr/bin/gnuplot -e "dat_prefix='output/time_piece_'; outfile='output/time_piece.png'; xlabelname='piece'" plot.plg
+/usr/bin/gnuplot -e "dat_prefix='output/time_piece_'; outfile='output/time_piece.png'; xlabelname='piece'; algo='$1'" plot.plg
