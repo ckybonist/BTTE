a3f23b3ca51fced0bf49426cd99d00bcaa203817
ChuKuanYu <joechu1592@gmail.com>
2014-12-29 22:33:35 +0800

    Fix scripts

--- script/init_dirs.sh
+++ script/init_dirs.sh
@@ -6,24 +6,6 @@ if [ $(basename $PROJ_ROOT) != 'BTTE' ]; then
 fi
 
 BUILD_DIR="$PROJ_ROOT/build"
-LOG_DIR="$PROJ_ROOT/log"
-PLOT_DIR="$PROJ_ROOT/plot/"
-
-if [ ! -d $LOG_DIR ]; then
-    mkdir $LOG_DIR
-fi
-
-if [ ! -d $PLOT_DIR/output ]; then
-    mkdir $PLOT_DIR/output
-fi
-
-if [ ! -d $PLOT_DIR/time_piece ]; then
-    mkdir $PLOT_DIR/time_piece
-fi
-
-if [ ! -d $PLOT_DIR/time_peer ]; then
-    mkdir $PLOT_DIR/time_peer
-fi
 
 if [ ! -d $BUILD_DIR ]; then
     mkdir -p $BUILD_DIR/debug $BUILD_DIR/release
