55c13a60118e953e37ef80b0b72e55bb6dd24ba9
KuanYu Chu <joechu1592@gmail.com>
2015-01-06 12:36:38 +0800

    Add comment for algorithm

--- src/algorithm/rf_piece_selection.h
+++ src/algorithm/rf_piece_selection.h
@@ -7,6 +7,29 @@
 namespace btte_piece_selection
 {
 
+/*
+ * Current Implementation for example:
+ *
+ * Suppose a peer need these pieces: {1, 2, 3, 4, 5, 6, 7},
+ *
+ * and each piece own by number-of-neighbors (represent as count):
+ * <piece>    <count>
+ * 1          3
+ * 2          4
+ * 3          4
+ * 4          4
+ * 5          5
+ * 6          5
+ * 7          5
+ *
+ * Then it will first select piece #1 which have least count,
+ * then choose the second less (count is 4), but piece #{2,3,4}
+ * have same count, so we randomly choose one. Finally, we randomly
+ * choose one piece from {5,6,7}.
+ *
+ * So the piece selection result will probably be pieces #{1, 3, 6}.
+ *
+ * */
 class RarestFirst : public IPieceSelection
 {
   public:
